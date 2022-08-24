22396001ff7107e7ad32192b82fc3ca4032df7a1
Renan Storrer <storrerrenan@gmail.com>
2022-08-24 03:19:37 -0300

    se tudo deu certo, requisito 3 ok :D

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
+  // state = {
+  //   cardName: '',
+  //   cardDescription: '',
+  //   cardAttr1: '0',
+  //   cardAttr2: '0',
+  //   cardAttr3: '0',
+  //   cardImage: '',
+  //   cardRare: 'normal',
+  //   cardTrunfo: false,
+  //   isSaveButtonDisabled: true,
+  //   saveCard: [],
+  //   hasTrunfo: false,
+  // };
+
   render() {
     return (
       <div>
         <h1>Tryunfo</h1>
         <Form />
+        <Card />
       </div>
     );
   }
